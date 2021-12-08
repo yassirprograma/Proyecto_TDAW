21b6c94f4d0c28551310e71acc47550619a56396
Yassir mASTERRcpp <dj.jicara@gmail.com>
2021-12-08 21:30:14 +0000

    Angelo subió la plantilla de FAQ (para páginas de tipo listado)y yo subí el index con el menú

--- index.php
+++ index.php
@@ -1,9 +1,115 @@
-<html>
-  <head>
-    <title>IBM</title>
-  </head>
-  <body>
-    
-  </body>
-</html>
+<!DOCTYPE html>
+<html lang="es">
+      <head>
+            <meta charset="utf-8">
+            <title>Prueba formularios</title>
+            <link rel="stylesheet" type="text/css" href="./estilosindex/estilo_index.css">
+           
+      </head>
 
+      <body>
+      
+            <div id="cabecera" >
+                  <div id="logoibm">
+                        <a href="./"><img src="./imagenesindex/IBMLOGO.png" alt="LOGO DE LA PÁGINA"></a>                        
+                  </div>
+
+                  <nav id="menu">
+                        <ul id="elementosmenu">                         
+                              <li class="desplegable"> 
+                                    Sobre IBM
+                                    <ul>
+                                          <li>
+                                                <a href="./paginas/QueEsIBM/QueEsIBM.html" target="iframecontenido" >¿Qué es IBM?</a>
+                                          </li>
+                                          <li>
+                                                <a href="./paginas/HistoriaIBM/HistoriaIBM.html" target="iframecontenido">Historia de IBM</a>
+                                          </li>
+                                    </ul>
+                              </li>
+
+                              <li>
+                                    <a href="./paginas/FAQ/FAQ.html" target="iframecontenido">FAQ</a>
+                              </li>
+
+                              <li>
+                                    <a href="./paginas/ProductosServicios/ProductosServicios.html" target="iframecontenido">Productos y servicios</a>
+                              </li>
+
+                              <li class="desplegable">
+                                    IBM para estudiantes
+                                    <ul>
+                                          <li><a href="./paginas/OportunidadesEstudiantes/OportunidadesEstudiantes.html" target="iframecontenido">Oportunidades para estudiantes</a></li>
+                                          <li><a href="./paginas/Herramientas/Herramientas.html" target="iframecontenido">Herramientas</a></li>
+                                          <li><a href="./paginas/CapacitacionEducativo/CapacitacionEducativo.html" target="iframecontenido">Capacitación y contenido educativo</a></li>
+                                          <li><a href="./paginas/EscomIBM/EscomIBM.html" target="iframecontenido">ESCOM e IBM</a></li>
+                                    </ul>
+                              </li>
+
+                              <li>
+                                    <a href="./paginas/EmpleoIBM/EmpleoIBM.html" target="iframecontenido">Empleo en IBM</a>
+                              </li>
+
+                              <li>
+                                    <a href="./paginas/ComunidadIBM/ComunidadIBM.php" target="iframecontenido">Comunidad IBM</a>
+                              </li>
+
+                              <li>
+                                    <a href="./paginas/EventosIBM/EventosIBM.html" target="iframecontenido">Eventos IBM</a>
+                              </li>
+                                                                                          
+                        </ul>                  
+                  </nav>
+
+                  <div id="login">
+                        <a id="botonlogin" href="./paginas/InicioSesion/InicioSesion.php">
+                              <img src="./imagenesindex/usericono.png" alt="icono de inicio de sesión"> 
+                              <br>
+                              Inicio de sesión
+                        </a>
+                  </div>
+                  
+                  
+            </div>
+
+
+
+
+
+
+            <!--Aquí se manda el contenido principal-->
+            <div id="contenidoprincipal">
+                  <iframe id="iframecontenido" name="iframecontenido" src="./paginas/IBMHome/IBMHome.html" >
+
+                  </iframe>
+            </div>
+
+            
+
+
+
+
+
+            <div id="piedepagina">      
+                  <div id="DatosContacto">
+                  
+                  </div>
+
+                  <div id="logos">
+
+                  </div>
+
+                  <div id="enlaces">
+
+                  </div>
+
+            </div>
+
+
+
+
+
+
+
+      </body>
+</html>
